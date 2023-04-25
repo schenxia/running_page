3b3f4e93ed6f6e6eb38247bbdfffa322ca81e134
schenxia <zitachen001@gmail.com>
2023-04-24 21:16:26 -0700

    Add details for Mengke

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   pathPrefix: process.env.PATH_PREFIX || '/',
   siteMetadata: {
-    siteTitle: 'Mengke's Running Page',
+    siteTitle: "Mengke's Running Page",
     siteUrl: 'https://schenxia.github.io/running_page/',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',
