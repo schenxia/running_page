b08c43ec90f3803e9afbeeaf8d65fd82922b07d4
schenxia <45961727+schenxia@users.noreply.github.com>
2024-01-31 19:16:21 -0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,7 +10,7 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Mengke's Running Page',
+  siteTitle: 'Mengke Running Page',
   siteUrl: 'https://schenxia.github.io/running_page/',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
